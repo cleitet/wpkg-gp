14f3ec4ccd373a2ed30960f944e9728cd77096fe
cleitet@gmail.com <cleitet@gmail.com@1034c5a4-fba7-11de-91df-05c19d2928fe>
2010-12-21 09:40:45 +0000

    Modified a bit for debugging. It is not used anymore, as wpkg-gp.dll now is a pipe client instead of calling this.
    Useful for debugging though.

--- src/WpkgPipeClient.py
+++ src/WpkgPipeClient.py
@@ -15,10 +15,14 @@ import win32api
 import winerror
 import sys, os, traceback
 
-verbose = 0
+debug = 0
     
 def runClient(server,msg,debug=False):
-    pipeHandle = CreateFile("\\\\%s\\pipe\\WPKG" % server, GENERIC_READ|GENERIC_WRITE, 0, None, OPEN_EXISTING, 0, None)
+    try:
+        pipeHandle = CreateFile("\\\\%s\\pipe\\WPKG" % server, GENERIC_READ|GENERIC_WRITE, 0, None, OPEN_EXISTING, 0, None)
+    except pywintypes.error, (n, f, e):
+        print "Error when generating pipe handle: %s" % e
+        return 1
     SetNamedPipeHandleState(pipeHandle, PIPE_READMODE_MESSAGE, None, None)
     WriteFile(pipeHandle, msg)
     msg = ""
@@ -29,34 +33,40 @@ def runClient(server,msg,debug=False):
                 print (readmsg)
             else: #Strip 3 digit status code
                 print (readmsg[4:])
-            sys.stdout.flush()
         except win32api.error as exc:
             if exc.winerror == winerror.ERROR_PIPE_BUSY:
                 win32api.Sleep(5000)
                 continue
             break
+            
+        sys.stdout.flush()
+    
+    sys.stdout.flush()
+    print "1"
+    sys.stdout.flush()
+    return 0
     
 
 def main():
     import sys, getopt
     server = "."
     try:
-        opts, args = getopt.getopt(sys.argv[1:], 's:vl')
+        opts, args = getopt.getopt(sys.argv[1:], 's:dl')
         for o,a in opts:
             if o=='-s':
                 server = a
-            if o=='-v':
-                global verbose
-                verbose = 1
+            if o=='-d':
+                global debug
+                debug = 1
         msg = " ".join(args).encode("mbcs")
     except getopt.error as msg:
         print(msg)
         my_name = os.path.split(sys.argv[0])[1]
         print("Usage: %s [-v] [-s server]" % my_name)
-        print("       -v = verbose")
+        print("       -d = debug")
         return
     
-    runClient(server, msg)
+    return runClient(server, msg)
 
 if __name__=='__main__':
     main()
