078909fcccd3eb253d3096b77d7a6a7da797cc0d
cleitet@gmail.com <cleitet@gmail.com@1034c5a4-fba7-11de-91df-05c19d2928fe>
2010-12-30 13:55:21 +0000

    Bumped to 0.7

--- make_installer.cmd
+++ make_installer.cmd
@@ -1,5 +1,5 @@
 @echo off
-set VERSION=0.6
+set VERSION=0.7
 
 cd %~dp0
 
@@ -7,5 +7,5 @@ c:\python26\python.exe setup.py py2exe
 "%ProgramFiles%\Windows Installer XML v3\bin\candle.exe" -arch x86 -dReleaseVersion=%VERSION% -out installer\x86\wpkg-gp-%VERSION%_x86.wixobj wpkg-gp.wxs
 "%ProgramFiles%\Windows Installer XML v3\bin\candle.exe" -arch x64 -dReleaseVersion=%VERSION% -out installer\x64\wpkg-gp-%VERSION%_x64.wixobj wpkg-gp.wxs
 
-"%ProgramFiles%\Windows Installer XML v3\bin\light.exe" -spdb -ext WixUiExtension installer\x86\wpkg-gp-0.6_x86.wixobj
-"%ProgramFiles%\Windows Installer XML v3\bin\light.exe" -spdb -ext WixUiExtension installer\x64\wpkg-gp-0.6_x64.wixobj
+"%ProgramFiles%\Windows Installer XML v3\bin\light.exe" -spdb -ext WixUiExtension installer\x86\wpkg-gp-%VERSION%_x86.wixobj
+"%ProgramFiles%\Windows Installer XML v3\bin\light.exe" -spdb -ext WixUiExtension installer\x64\wpkg-gp-%VERSION%_x64.wixobj
