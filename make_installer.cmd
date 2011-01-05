b3b5444910fc1afdee7cb72a3a15c6cf35b4bd2e
cleitet@gmail.com <cleitet@gmail.com@1034c5a4-fba7-11de-91df-05c19d2928fe>
2011-01-05 13:43:05 +0000

    make_installer.cmd: - Bumped for version 0.8 - Added code for extra modularity in .wxs files
    wpkg-gp.wxs: 
    - Bumped GUID for 0.8
    - Added files needed for encryption of Network User password
    - Renamed Wpkg-Gp-Client to Wpkg-Gp-test.exe
    - Added a CustomAction for updating the Network User username and password
    - Added references to .wxs modules

--- make_installer.cmd
+++ make_installer.cmd
@@ -1,11 +1,13 @@
 @echo off
-set VERSION=0.7
+set VERSION=0.8
 
 cd %~dp0
 
 c:\python26\python.exe setup.py py2exe
-"%ProgramFiles%\Windows Installer XML v3\bin\candle.exe" -arch x86 -dReleaseVersion=%VERSION% -out installer\x86\wpkg-gp-%VERSION%_x86.wixobj wpkg-gp.wxs
-"%ProgramFiles%\Windows Installer XML v3\bin\candle.exe" -arch x64 -dReleaseVersion=%VERSION% -out installer\x64\wpkg-gp-%VERSION%_x64.wixobj wpkg-gp.wxs
+del /s /q installer\x86\*.wixobj
+del /s /q installer\x64\*.wixobj
+"%ProgramFiles%\Windows Installer XML v3\bin\candle.exe" -arch x86 -dReleaseVersion=%VERSION% -out installer\x86\ wpkg-gp.wxs installer\*.wxs
+"%ProgramFiles%\Windows Installer XML v3\bin\candle.exe" -arch x64 -dReleaseVersion=%VERSION% -out installer\x64\ wpkg-gp.wxs installer\*.wxs
 
-"%ProgramFiles%\Windows Installer XML v3\bin\light.exe" -spdb -ext WixUiExtension installer\x86\wpkg-gp-%VERSION%_x86.wixobj
-"%ProgramFiles%\Windows Installer XML v3\bin\light.exe" -spdb -ext WixUiExtension installer\x64\wpkg-gp-%VERSION%_x64.wixobj
+"%ProgramFiles%\Windows Installer XML v3\bin\light.exe" -spdb -ext WixUiExtension -out wpkg-gp-%VERSION%_x86.msi installer\x86\*.wixobj
+"%ProgramFiles%\Windows Installer XML v3\bin\light.exe" -spdb -ext WixUiExtension -out wpkg-gp-%VERSION%_x64.msi installer\x64\*.wixobj
