5b47e010b45452e4b7a487e393aab33d4e7eff03
cleitet@gmail.com <cleitet@gmail.com@1034c5a4-fba7-11de-91df-05c19d2928fe>
2011-09-08 08:35:28 +0000

    Testing branch

---
+++ shared/wpkg_common.h
@@ -0,0 +1,17 @@
+#include <Windows.h>
+#include <userenv.h>
+#include <wchar.h>
+#define BUFSIZE 2048
+#define bzero(b, len) (memset((b), '\0', (len)), (void) 0)
+#define wbzero(b, len) (wmemset((b), L'\0', (len)), (void) 0)
+
+#define LOG_INFO    1
+#define LOG_ERROR   2
+#define NO_LOG		3
+#define GPE 1
+#define EXE 2
+
+extern PFNSTATUSMESSAGECALLBACK gStatusCallback;
+void UpdateStatus(int status_type, wchar_t* message, int errorCode);
+DWORD executeWpkgViaPipe(int called_by, bool debug_flag);
+void debug(const wchar_t* debugMessage);
--- src/gpe/shared/MessageFile.mc
+++
@@ -1,6 +0,0 @@
-
-MessageId=100
-SymbolicName=EVMSG_STDMSG
-Language=English
-%1
-.
